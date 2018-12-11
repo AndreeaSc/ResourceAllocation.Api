c5cfa3572cf11e50e37f3f745a09acaac83037bf
Andreea Schiau <schiau.andreea@gmail.com>
2018-12-11 10:09:26 +0200

    update DesignerEntity (added properties)

--- ResourceAllocation.Domain/DesignerEntity.cs
+++ ResourceAllocation.Domain/DesignerEntity.cs
@@ -7,5 +7,8 @@ namespace ResourceAllocation.Domain
     public class DesignerEntity : BaseEntity
     {
         public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
     }
 }
