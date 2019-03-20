b29057785cc5a0cf5859639c70fff3dc1fc3b041
Andreea Schiau <schiau.andreea@gmail.com>
2019-03-20 10:40:13 +0200

    add missing files

---
+++ ResourceAllocation.DataLayer/Artists/IArtistsRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using ResourceAllocation.Domain;
+
+namespace ResourceAllocation.DataLayer.Artists
+{
+    public interface IArtistsRepository
+    {
+        void Add(Artist entity);
+        void Delete(Guid id);
+        IEnumerable<Artist> GetAll();
+        Artist GetById(Guid id);
+        void Update(Artist entity);
+    }
+}
---
+++ ResourceAllocation.Domain/CommonArtistEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ResourceAllocation.Domain
+{
+    public class CommonArtistEntity
+    {
+        public Guid FirstDesigner { get; set; }
+        public Guid SecondDesigner { get; set; }
+        public Guid ArtistId { get; set; }
+    }
+}
